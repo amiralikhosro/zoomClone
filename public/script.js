bdd41a90df1d928a646c0f5f9b85054e01363b37
ali.khosro72@gmail.com <ali.khosro72@gmail.com>
2023-01-03 10:13:21 +0330

    feat: publish the user connected stream

--- public/script.js
+++ public/script.js
@@ -38,6 +38,10 @@ navigator.mediaDevices
 
     myPeer.on("call", (call) => {
       call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
     });
 
     socket.on("user-connected", (userId) => {
